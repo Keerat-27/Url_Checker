7f03804f2db3b27919dd1ae76677d36a17e5e2fa
KEERAT SINGH KHURANA <75303995+Keerat-27@users.noreply.github.com>
2022-07-02 15:11:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,96 +1,111 @@
-import express, { json } from 'express';
-import fetch from 'node-fetch';
-import fs from 'fs';
+import express, { json } from "express";
+import fetch from "node-fetch";
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
 
 async function fetchURLStatus(urlArray, linksData, callback) {
-    var id, urlName, statusCode, statusText;
-    const date = new Date();
-    var jsonString = {}
-    urlArray = urlArray.split("\n");
-    for (let pos in urlArray) {
-        await fetch(urlArray[pos], { method: 'GET' })
-            .then(res => {
-                id = "u" + pos;
-                urlName = urlArray[pos];
-                statusCode = res.status;
-                statusText = res.statusText;
-            })
-            .catch(error => {
-                id = "u" + pos;
-                urlName = urlArray[pos];
-                statusCode = "Invalid URL";
-                statusText = "The URL was invalid / not found";
-            });
-        jsonString = { ...jsonString, [id]: { url: urlName, status: statusCode, statusText: statusText, date: date } }
+  var id, urlName, statusCode, statusText;
+  const date = new Date();
+  var jsonString = {};
+  urlArray = urlArray.split("\n");
+  for (let pos in urlArray) {
+    if (isValidHttpUrl(urlArray[pos])) {
+      await fetch(urlArray[pos], { method: "GET" })
+        .then((res) => {
+          id = "u" + pos;
+          urlName = urlArray[pos];
+          statusCode = res.status;
+          statusText = res.statusText;
+        })
+        .catch((error) => {
+          id = "u" + pos;
+          urlName = urlArray[pos];
+          statusCode = "Invalid URL";
+          statusText = "The URL was invalid / not found";
+        });
+      jsonString = {
+        ...jsonString,
+        [id]: {
+          url: urlName,
+          status: statusCode,
+          statusText: statusText,
+          date: date,
+        },
+      };
     }
-    // console.log(jsonString)
-    saveOutput(jsonString);
-    callback(linksData)
-};
+  }
+  // console.log(jsonString)
+  saveOutput(jsonString);
+  callback(linksData);
+}
 
 function saveOutput(jsonString) {
-    fs.writeFile("result.json", JSON.stringify(jsonString), function (err) {
-        if (err) throw err;
-        console.log('File Saved');
-    });
+  fs.writeFile("result.json", JSON.stringify(jsonString), function (err) {
+    if (err) throw err;
+    console.log("File Saved");
+  });
 }
-
 
 const readData = async (callback) => {
-    var linksData = "", listOfLinks = "";
-    try {
-        console.log(5);
-        let rawdata = fs.readFileSync('result.json');
-        linksData = JSON.parse(rawdata) || "";
-        
+  var linksData = "",
+    listOfLinks = "";
+  try {
+    console.log(5);
+    let rawdata = fs.readFileSync("result.json");
+    linksData = JSON.parse(rawdata) || "";
+  } catch (err) {
+    linksData = "";
+    console.log("File not found or insufficient read/write permissions");
+  } finally {
+    await fs.readFile("listOfLinks.txt", "utf8", function (err, data) {
+      if (err) throw err;
+      listOfLinks = data;
+      // console.log(typeof (listOfLinks))
+      console.log(4);
+      callback(listOfLinks, linksData);
+    });
+  }
+  console.log(2);
 
-    }
-    catch (err) {
-        linksData = "";
-        console.log("File not found or insufficient read/write permissions");
-    }
+  // console.log(listOfLinks)
+};
 
-    finally {
-        await fs.readFile("listOfLinks.txt", 'utf8', function (err, data) {
-            if (err) throw err;
-            listOfLinks = data;
-            // console.log(typeof (listOfLinks))
-            console.log(4);
-            callback(listOfLinks, linksData);
+app.post("/read", (req, res) => {
+  // console.log("HEHE");
+  const callbackNo2 = (linksData) => {
+    console.log(3);
 
-        });
-    }
-    console.log(2)
-
-    // console.log(listOfLinks)
-
-}
-
-app.post('/read', (req, res) => {
-    // console.log("HEHE");
-    const callbackNo2 = (linksData) => {
-        console.log(3);
-
-        res.send(linksData);
-        res.end();
-    }
-    const callback = (listOfLinks, linksData) => {
-        console.log(1);
-        fetchURLStatus(listOfLinks, linksData, callbackNo2);
-    }
-    readData(callback);
+    res.send(linksData);
+    res.end();
+  };
+  const callback = (listOfLinks, linksData) => {
+    console.log(1);
+    fetchURLStatus(listOfLinks, linksData, callbackNo2);
+  };
+  readData(callback);
 });
-app.get('/', function (req, res) {
-    // res.send('<script>window.location.replace("./read");</script>');
-    res.sendFile("index.html", { root: '.' });
+app.get("/", function (req, res) {
+  // res.send('<script>window.location.replace("./read");</script>');
+  res.sendFile("index.html", { root: "." });
 });
-app.use(express.static('./'))
+app.use(express.static("./"));
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log("Server started at http://localhost:" + port);
