7742b55242831872a3651ab309f420699a8d9c59
KEERAT SINGH KHURANA <75303995+Keerat-27@users.noreply.github.com>
2022-07-04 14:18:58 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,22 @@ function addItem(event) {
     if (typeof (event) == 'string' && event !== "") {
         let linksDataObj = JSON.parse(linksData);
         let divElement = '';
+        let count = 1;
+        let nameList = ["Jmc","Sggscc","Miranda House","Shivaji College","ANDC","Aurobindo College","Gargi College","Kalindi College","Vasanta College","Dyal Singh College","Holy Family College"
+        ,"Hansraj College","Placement Cell Hansraj","Hansraj Faculty","Hansraj Alumni","SRCC Smartprof","LSR Smartprof","Lady Reading School","LAHDC SSRB","Ladakh Heli Services","Langham Capital"
+        ,"Delhi EV","Delhi EV Incentive","Ladakh Connect","LEH Permit","Sakoon","Vaishno Devi","District Udham Nagar","Ladakh EYE","Motor Vehicle Department"];
         for (var items in linksDataObj) {
             let { url, status, statusText, date } = linksDataObj[`${items}`];
-            // console.log(url, status, statusText, date);
             dateObj = new Date(date).toLocaleString("en-US");
-            // console.log(dateObj)
             if(status == 200)
-                divElement = `<div class= "green"></div> ${status}`
+                divElement = `<div class= "green"></div>`
             else
                 divElement = `<div class= "red"></div> ${status}`
 
             tBody.innerHTML += `<tr>
             <!-- <th scope="row"></th> -->
+            <td>${count++}</td>
+            <td>${nameList[count-2]}</td>
             <td>${url}</td>
             <td>${statusText}</td>
             <td>${divElement}</td>   
@@ -34,12 +38,17 @@ var linksData = "";
 
 const readData = () => {
     const xhttp = new XMLHttpRequest();
+    $(".progress-bar").animate({
+        width: "70%",
+    }, 500);
     xhttp.onload = function () {
         readLinks = true;
         linksData = this.responseText;
-        addItem(linksData);
-        //console.log("Reci")
+        $(".progress-bar").animate({
+            width: "100%",
+        }, addItem(linksData));
     }
+    
     xhttp.open("POST", "./read");
     xhttp.send();
 }
